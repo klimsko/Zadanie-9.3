ba21a51cb962ea16d818ff23946a98ef4ba1814c
klimsko <genek.klim@gmail.com>
2017-03-08 18:04:18 +0100

    karuzela gotowa na 50%

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
-$('span').each(function (index, element) {
-  if (index % 2){
-    $(element).css('color', 'red');
+$(function(){
+  //var li = $('li').;
+  //console.log(li);
+  var carouselList = $('#carousel ul');
+  var interval = null;
+
+  interval = setInterval(changeSlides, 3000);
+
+  function changeSlides(){
+    carouselList.animate({"marginLeft": -400}, 1500, moveFirstSlide);
   }
-});
-$('span:even').css('color', 'blue');
+  function moveFirstSlide(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  }
+  function moveLastSlide(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    firstItem.after(lastItem);
+    carouselList.css({marginLeft:0});
+  }
+  
+  carouselList.mouseover(function() {
+    clearInterval(interval);
+  })
+    .mouseout(function() {
+      interval = setInterval(changeSlides, 3000);
+    });
 
-var paragraphs = $('p:even');
-paragraphs.each(function(index, element) {
+  $('.leftButton, .rightButton').mouseover(function(){
+    $(this).css('font-size', '2em');
+  })
+  $('.leftButton, .rightButton').mouseout(function(){
+    $(this).css('font-size', '1em');
+  })
+  $('.leftButton').click(function(){
+    var li = carouselList.find("li");
+    carouselList.css("margin-left", "-400");
+    li.first().before(li.last());
+    carouselList.animate({"marginLeft": 0}, 1500, function(){
 
-var button = '<button class="btn" data-tmp="' + (index+1) + '">Click me</button>'
-$(element).append(button); 
-
-});
-
-$("button").click(function(){
-  alert($(this).attr("data-tmp"));
+    });
+    clearInterval(interval);
+  })
+  $('.rightButton').click(function(){
+    var li = carouselList.find("li");
+    carouselList.animate({"marginLeft": -400}, 1500, function(){
+    li.last().after(li.first());
+    carouselList.css("margin-left", "0");
+    });
+    clearInterval(interval);
+  })
+  
 });
